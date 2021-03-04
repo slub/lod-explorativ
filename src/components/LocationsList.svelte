4f03218ed4a5a81dd554539b6831ccb0970be3b2
Mirko de Almeida Madeira Clemente <contact@mirkoclemente.net>
2021-03-04 18:12:55 +0100

    added location list component

---
+++ src/components/LocationsList.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { Geo } from 'types/es';
+
+  export let locations: Geo[];
+</script>
+
+<div class="container">
+  <h4>Erwähnte Orte</h4>
+  <ul class="authorList">
+    {#each locations as geo}
+      <li>{geo.preferredName}</li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .container {
+    margin-bottom: 2rem;
+  }
+</style>
