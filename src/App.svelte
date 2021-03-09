eab91e7e65e68152dfcb27f65373a3e938bef774
Mirko de Almeida Madeira Clemente <contact@mirkoclemente.net>
2021-03-09 16:47:59 +0100

    prepared graph component

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
   export let name: string;
 
-  import TopicList from "./components/TopicList.svelte";
-  import SearchInput from "./components/SearchInput.svelte";
+  import TopicList from './components/TopicList.svelte';
+  import SearchInput from './components/SearchInput.svelte';
+  import Graph from './components/Graph.svelte';
 </script>
 
 <main>
   <h1>{name}</h1>
   <SearchInput />
+  <Graph />
   <TopicList />
 </main>
 
--- src/state/uiState.ts
+++ src/state/uiState.ts
@@ -1,3 +1,3 @@
 import { writable } from 'svelte/store';
 
-export const query = writable('Weltpolitik');
+export const query = writable('Kunst');
