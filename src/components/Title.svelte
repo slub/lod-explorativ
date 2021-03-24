019a147c3f5f0480ab835d9180537635cc21e7ee
Mirko de Almeida Madeira Clemente <contact@mirkoclemente.net>
2021-03-24 17:04:09 +0100

    added Title component

---
+++ src/components/Title.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let name = 'title';
+
+  function handleTitleClick() {
+    window.location.href = window.location.origin;
+  }
+</script>
+
+<h1 class="title" on:click={handleTitleClick}>{name}</h1>
+
+<style>
+  .title {
+    cursor: pointer;
+  }
+</style>
