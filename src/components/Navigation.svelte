4342667d93c7ae74cf5fbe91d5aca0b632e87fda
Mirko de Almeida Madeira Clemente <contact@mirkoclemente.net>
2021-05-27 16:22:24 +0200

    reset restrict param if topic is selected in navigation

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-  import { query } from '../state/uiState';
+  import { query, queryExtension } from '../state/uiState';
   import { additionalTypes } from '../state/dataAPI';
 
   function handleClick(q) {
+    queryExtension.set(null);
     query.set(q);
   }
 </script>
